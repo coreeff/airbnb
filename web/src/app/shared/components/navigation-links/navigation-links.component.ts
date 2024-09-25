38b9a7aa1361ff54d5aeaa7dc308320d1c1b6bdf
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-09-25 11:54:22 +0545

    updated homepage

--- web/src/app/shared/components/navigation-links/navigation-links.component.ts
+++ web/src/app/shared/components/navigation-links/navigation-links.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   standalone: true,
