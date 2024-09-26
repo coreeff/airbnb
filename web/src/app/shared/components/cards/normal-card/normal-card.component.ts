4ddf700cc4f42f7b281fb2d94632c88daf6db901
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-09-26 11:53:57 +0545

    updated homepage macro details

--- web/src/app/shared/components/cards/normal-card/normal-card.component.ts
+++ web/src/app/shared/components/cards/normal-card/normal-card.component.ts
@@ -4,13 +4,22 @@ import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { DecimalPrecisionPipe } from '@/app/pipes/decimal-precision.pipe';
+import { ButtonComponent } from '../../button/button.component';
 
 @Component({
   standalone: true,
   selector: 'app-normal-card',
   templateUrl: './normal-card.component.html',
-  imports: [CommonModule, RouterLink, DecimalPrecisionPipe],
+  imports: [CommonModule, RouterLink, DecimalPrecisionPipe, ButtonComponent],
 })
 export class NormalCardComponent {
   @Input() room!: Property;
+
+  onButtonClick(event: MouseEvent) {
+    event.stopPropagation(); // Stops the event from propagating to the link
+    event.preventDefault(); // Prevents the link's default behavior (optional)
+
+    // Perform your button action here
+    console.log('Button clicked!');
+  }
 }
