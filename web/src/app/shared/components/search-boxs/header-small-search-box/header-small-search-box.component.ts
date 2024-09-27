b0d7259f5870278a00aa4d93121ca02cba519960
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-09-27 16:22:31 +0545

    updated the header toggler styles

--- web/src/app/shared/components/search-boxs/header-small-search-box/header-small-search-box.component.ts
+++ web/src/app/shared/components/search-boxs/header-small-search-box/header-small-search-box.component.ts
@@ -23,15 +23,15 @@ export class HeaderSmallSearchBoxComponent {
     this.expandChange.emit(this.expand);
   }
 
-  @HostListener('document:click', ['$event'])
-  onClickOutside(event: Event) {
-    const target = event.target as HTMLElement;
-    const isInsideSmallBox = target.closest('app-header-small-search-box');
-    const isInsideLargeBox = target.closest('app-header-large-search-box');
+  // @HostListener('document:click', ['$event'])
+  // onClickOutside(event: Event) {
+  //   const target = event.target as HTMLElement;
+  //   const isInsideSmallBox = target.closest('app-header-small-search-box');
+  //   const isInsideLargeBox = target.closest('app-header-large-search-box');
 
-    if (!isInsideSmallBox && !isInsideLargeBox) {
-      this.expand = false;
-      this.expandChange.emit(this.expand);
-    }
-  }
+  //   if (!isInsideSmallBox && !isInsideLargeBox) {
+  //     this.expand = false;
+  //     this.expandChange.emit(this.expand);
+  //   }
+  // }
 }
