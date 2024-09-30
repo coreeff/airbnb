9934ca4c832d90908a2ff6adbf2c633ab973fbc1
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-09-30 15:45:53 +0545

    feat: upload service for api

--- apps/api/src/common/upload/upload.controller.ts
+++ apps/api/src/common/upload/upload.controller.ts
@@ -1,9 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Post,
+  HttpStatus,
+  Controller,
+  UploadedFile,
+  HttpException,
+  UseInterceptors,
+} from '@nestjs/common';
+import { UploadService } from './upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('upload')
 export class UploadController {
-  @Get('')
-  uploadFile() {
-    return 'File uploaded successfully';
+  constructor(private readonly uploadService: UploadService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file)
+      throw new HttpException('File not found', HttpStatus.BAD_REQUEST);
+
+    const fileUrl = await this.uploadService.uploadFileUsingFormData(file);
+
+    return { url: fileUrl };
   }
 }
--- apps/api/src/common/upload/upload.service.ts
+++ apps/api/src/common/upload/upload.service.ts
@@ -1,10 +1,44 @@
+import { Readable } from 'stream';
+import * as FormData from 'form-data';
 import { Injectable } from '@nestjs/common';
+
+import axios from 'axios';
 import cloudinary from './config/cloudinary.config';
 
 @Injectable()
 export class UploadService {
-  async uploadFile(file: Express.Multer.File) {
-    const result = await cloudinary.uploader.upload(file.path);
+  // @TODO: to fix the sdk upload
+  async uploadFileUsingSDK(file: Express.Multer.File): Promise<string> {
+    const fileStr = `data:${file.mimetype};base64,${file.buffer.toString('base64')}`;
+
+    const result = await cloudinary.uploader.upload(fileStr, {
+      resource_type: 'auto',
+    });
+
     return result.secure_url;
+  }
+
+  async uploadFileUsingFormData(file: Express.Multer.File) {
+    const formData = new FormData();
+    formData.append('file', Readable.from(file.buffer), file.originalname);
+    formData.append('upload_preset', `${process.env.CLOUDINARY_UPLOAD_PRESET}`);
+
+    const imageUploadUrl: string = `${process.env.CLOUDINARY_IMAGE_UPLOAD_LINK}/image/upload`;
+
+    try {
+      const uploadResponse = await axios.post(imageUploadUrl, formData, {
+        headers: {
+          ...formData.getHeaders(),
+        },
+      });
+
+      const uploadedImageData = uploadResponse.data;
+      const imageUrl = uploadedImageData.secure_url;
+
+      return imageUrl;
+    } catch (error) {
+      console.error('Failed to upload file:', error);
+      throw new Error('Failed to upload file');
+    }
   }
 }
