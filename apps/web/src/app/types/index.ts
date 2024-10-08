6da1627acf3dc284d8300f6c2b64b9553b4e8504
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-10-08 15:26:23 +0545

    updated listing types

--- apps/web/src/app/types/index.ts
+++ apps/web/src/app/types/index.ts
@@ -27,28 +27,53 @@ export interface OptionsI {
     | boolean;
 }
 
+// export type Property = {
+//   id: number;
+//   documentId: string;
+//   location: string;
+//   price: number;
+//   rating: number;
+//   image_url: string;
+//   createdAt: string;
+//   updatedAt: string;
+//   publishedAt: string;
+//   locale: string | null;
+//   date_from: string;
+//   date_to: string;
+// };
+
 export type Property = {
-  id: number;
-  documentId: string;
-  location: string;
+  id: string;
+  title: string;
+  description: string;
   price: number;
-  rating: number;
-  image_url: string;
-  createdAt: string;
-  updatedAt: string;
-  publishedAt: string;
-  locale: string | null;
-  date_from: string;
-  date_to: string;
+  bedrooms: number;
+  bathrooms: number;
+  sqft: number;
+  location: number;
+  availability_to: Date;
+  availability_from: Date;
+  average_rating: boolean;
+  created_at: Date;
+  updated_at: Date;
+  categoryId: string;
+  userId: string;
+  images: Images[];
+};
+
+type Images = {
+  id: string;
+  url: string;
+  created_at: Date;
+  updated_at: Date;
+  listingId: string;
 };
 
 type Meta = {
-  pagination: {
-    page: number;
-    pageSize: number;
-    pageCount: number;
-    total: number;
-  };
+  itemCount: number;
+  pageCount?: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
 };
 
 export type RoomsType = {
