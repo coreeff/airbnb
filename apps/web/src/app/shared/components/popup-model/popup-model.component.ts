145b6f26c8d971b8fd255d7fbc4e8bf3bb2e124f
ujjwalbhandarii <ujjwalbhandari14@gmail.com>
2024-10-16 22:42:33 +0545

    added form module

--- apps/web/src/app/shared/components/popup-model/popup-model.component.ts
+++ apps/web/src/app/shared/components/popup-model/popup-model.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-popup-model',
